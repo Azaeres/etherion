238df7da45a1fc91222386f22f07becf438092b5
Azaeres <azaeres@gmail.com>
2020-07-15 08:58:34 -0700

    Add redux-offline

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { offline } from 'redux-offline';
+import offlineConfig from 'redux-offline/lib/defaults';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  enhancers: [
+    offline(offlineConfig)
+  ]
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+declare module 'redux-offline';
+declare module 'redux-offline/lib/defaults';
+declare module 'redux-logger';
