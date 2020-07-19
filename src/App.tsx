023dda586f9f7675f6f9b97c4f70d0e0e9617534
Azaeres <azaeres@gmail.com>
2020-07-18 23:15:58 -0700

    Fixed typescript warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import PhaserGame from './features/game/PhaserGame';
 import './App.css';
 
