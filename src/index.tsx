68cc87a564af108dbca7e456be676a21617e2470
Azaeres <azaeres@gmail.com>
2020-07-20 05:19:30 -0700

    Phaser prod test

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { store } from './state/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+console.log('index  > process.env:', process.env);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
